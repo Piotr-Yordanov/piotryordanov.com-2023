bbff068cdc874936c697f9b18bceac016108f886
Piotr Yordanov <48773775+piotryordanov@users.noreply.github.com>
2021-01-14 22:02:39 +0200

    updates

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,5 @@
 import * as React from 'react'
 import Router from 'next/router'
-import { useState, useEffect } from 'react';
-import { useRouter } from 'next/router'
 import * as gtag from '../lib/gtag'
 import { AppProps } from 'next/app'
 import Meta from './meta'
@@ -16,25 +14,12 @@ import './styles/pattern.min.css';
 
 Router.events.on('routeChangeStart', () => NProgress.start())
 Router.events.on('routeChangeError', () => NProgress.done())
+Router.events.on('routeChangeComplete', () => NProgress.done())
 
 
 
 
 const App = ({ Component, pageProps }) => {
-  const router = useRouter()
-  useEffect(() => {
-    console.log('loading');
-    // gtag.pageview(location.pathname)
-
-    const handleRouteChange = (url) => {
-      NProgress.done()
-      // gtag.pageview(url)
-    }
-    router.events.on('routeChangeComplete', handleRouteChange)
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [router.events])
   return <div>
     <Meta />
     <Component {...pageProps} />
