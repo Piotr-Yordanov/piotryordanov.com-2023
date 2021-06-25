4c06b442cd5939b5ab978da22d2f713e081b8efd
Alpha <piotr@piotryordanov.com>
2021-06-25 18:45:43 +0200

    updates before next9 upgrades

--- next.config.js
+++ next.config.js
@@ -1,16 +1,16 @@
-const sitemap = require('nextjs-sitemap-generator');
+const sitemap = require("nextjs-sitemap-generator")
 sitemap({
-    baseUrl: '<your_website_base_url>',
-    pagesDirectory: __dirname + "/pages",
-    targetDirectory: 'static/'
-});
+  baseUrl: "piotryordanov.com",
+  pagesDirectory: __dirname + "/pages",
+  targetDirectory: "public/",
+})
 module.exports = {
-    exportPathMap: function () {
-        return {
-            '/': { page: '/' },
-            '/developper': { page: '/developper' },
-            '/scalper': { page: '/scalper' },
-            '/meditator': { page: '/meditator' },
-        }
+  exportPathMap: function () {
+    return {
+      "/": { page: "/" },
+      "/developper": { page: "/developper" },
+      "/scalper": { page: "/scalper" },
+      "/meditator": { page: "/meditator" },
     }
-};
+  },
+}
