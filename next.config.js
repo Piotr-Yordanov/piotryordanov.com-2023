c1a70338cc65fc30b2467c276aadf62355f86dd3
Alpha <piotr@piotryordanov.com>
2021-10-08 10:16:14 +0200

    ADded rewrites to nextconfig

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 const sitemap = require("nextjs-sitemap-generator")
+
 sitemap({
   baseUrl: "piotryordanov.com",
   pagesDirectory: __dirname + "/pages",
@@ -7,10 +8,26 @@ sitemap({
 module.exports = {
   exportPathMap: function () {
     return {
-      "/": { page: "/" },
-      "/developper": { page: "/developper" },
-      "/scalper": { page: "/scalper" },
-      "/meditator": { page: "/meditator" },
+      "/": {page: "/"},
+      "/developper": {page: "/developper"},
+      "/scalper": {page: "/scalper"},
+      "/meditator": {page: "/meditator"},
     }
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/about',
+        destination: '/',
+      },
+    ]
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/secondbrain',
+        destination: 'https://publish.obsidian.md/piotryordanov',
+      },
+    ]
   },
 }
